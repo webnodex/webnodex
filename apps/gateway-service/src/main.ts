67ae95478418b87242a6cc86a97342ec4026a9c1
Davide Di Criscito <davdevx.coder@outlook.com>
2025-09-01 08:27:13 +0200

    feat(@webnodex/gateway-service): add òwebnodex/gateway dep

--- apps/gateway-service/src/main.ts
+++ apps/gateway-service/src/main.ts
@@ -1,1 +1,9 @@
-console.log('Hello World');
+import { Gateway } from '@webnodex/gateway';
+
+const config = {
+  port: parseInt(process.env.PORT || '3000'),
+};
+
+const webnodex = new Gateway(config);
+
+webnodex.start();
