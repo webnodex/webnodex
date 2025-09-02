26d22699e7bd0a155e87b13709657f68c0407f5c
Davide Di Criscito <davdevx.coder@outlook.com>
2025-09-02 06:48:41 +0200

    fix(@webnodex/gateway-service): split build

--- apps/gateway-service/webpack.config.cjs
+++ apps/gateway-service/webpack.config.cjs
@@ -7,6 +7,13 @@ module.exports = {
     ...(process.env.NODE_ENV !== 'production' && {
       devtoolModuleFilenameTemplate: '[absolute-resource-path]',
     }),
+    library: {
+      type: 'module',
+    },
+    clean: true,
+  },
+  experiments: {
+    outputModule: true,
   },
   plugins: [
     new NxAppWebpackPlugin({
@@ -15,10 +22,48 @@ module.exports = {
       main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
       assets: ['./src/assets'],
-      optimization: false,
-      outputHashing: 'none',
-      generatePackageJson: true,
+      optimization: true,
+      outputHashing: 'contenthash',
+      generatePackageJson: false,
       sourceMaps: true,
+      vendorChunk: true,
+      namedChunks: true,
+      externalDependencies: 'none',
     }),
   ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 200000,
+      automaticNameDelimiter: '.',
+      cacheGroups: {
+        // Split vendors into individual chunks if they're big enough
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packagePath = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            );
+            if (!packagePath) return 'vendor.misc';
+            const packageName = packagePath[1].replace('@', '');
+            return `vendor.${packageName}`;
+          },
+          chunks: 'all',
+          reuseExistingChunk: true,
+          enforce: true,
+        },
+        // Group tiny vendors together
+        misc: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor.misc',
+          chunks: 'all',
+          priority: -10,
+          reuseExistingChunk: true,
+          enforce: false,
+        },
+      },
+    },
+  },
 };
